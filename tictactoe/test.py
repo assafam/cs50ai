b8d9a26699539f325ac9db1fb9e2b85b9971ad8e
Assaf Amitai <87387953+assafam@users.noreply.github.com>
2022-10-27 11:16:30 +0300

    Implement player, actions, result, winner, terminal Add tests

---
+++ tictactoe/test.py
@@ -0,0 +1,55 @@
+from tictactoe import initial_state, player, actions, result, winner, terminal
+import unittest
+
+class TestTTTMethods(unittest.TestCase):
+    def setUp(self):
+        self.board = initial_state()
+
+    def test_player(self):
+        self.assertEqual(player(self.board), "X")
+        self.board[0][0] = "X"
+        self.assertEqual(player(self.board), "O")
+
+    def test_actions(self):
+        self.assertEqual(len(actions(self.board)), 9)
+
+    def test_result(self):
+        self.board = result(self.board, (1, 0))
+        for i in range(3):
+            for j in range(3):
+                if i == 1 and j == 0:
+                    self.assertEqual(self.board[i][j], "X")
+                else:
+                    self.assertIsNone(self.board[i][j])
+        with self.assertRaises(RuntimeError):
+            self.board = result(self.board, (1, 0))
+
+    def fill_top_row_with_x(self):
+        for i in range(3):
+            self.board[0][i] = "X"
+
+    def fill_board_with_tie_result(self):
+        for i in range(3):
+            for j in range(3):
+                if (i == 0 and j == 0) or (i == 1 and j != 0) or (i == 2 and j != 2):
+                    self.board[i][j] = "X"
+                else:
+                    self.board[i][j] = "O"
+
+    def test_winner(self):
+        self.assertIsNone(winner(self.board))
+        self.fill_top_row_with_x()
+        self.assertEqual(winner(self.board), "X")
+        self.fill_board_with_tie_result()
+        self.assertIsNone(winner(self.board))
+
+    def test_terminal(self):
+        self.assertFalse(terminal(self.board))
+        self.fill_top_row_with_x()
+        self.assertTrue(terminal(self.board))
+        self.fill_board_with_tie_result()
+        self.assertTrue(terminal(self.board))
+
+
+if __name__ == '__main__':
+    unittest.main()
--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -3,6 +3,7 @@ Tic Tac Toe Player
 """
 
 import math
+from copy import deepcopy
 
 X = "X"
 O = "O"
@@ -22,36 +23,74 @@ def player(board):
     """
     Returns player who has the next turn on a board.
     """
-    raise NotImplementedError
+    flat_board = [cell for row in board for cell in row]
+    if flat_board.count(X) <= flat_board.count(O):
+        return X
+    else:
+        return O
 
 
 def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
     """
-    raise NotImplementedError
+    possible_actions = set()
 
+    for row in range(3):
+        for col in range(3):
+            if board[row][col] == EMPTY:
+                possible_actions.add((row, col))
+
+    return possible_actions
 
 def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
-    raise NotImplementedError
+    if board[action[0]][action[1]] != EMPTY:
+        raise RuntimeError('Invalid move')
 
+    new_board = deepcopy(board)
+    new_board[action[0]][action[1]] = player(board)
+    return new_board
 
 def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    raise NotImplementedError
+    # Check horizontal and vertical axes
+    for i in range(3):
+        if board[i][0] != EMPTY and board[i][0] == board[i][1] == board[i][2]:
+            return board[i][0]
+        if board[0][i] != EMPTY and board[0][i] == board[1][i] == board[2][i]:
+            return board[0][i]
+
+    # Check diagonal axes
+    if board[0][0] != EMPTY and board[0][0] == board[1][1] == board[2][2]:
+        return board[0][0]
+    if board[0][2] != EMPTY and board[0][2] == board[1][1] == board[2][0]:
+        return board[0][2]
+
+    # No winner found
+    return None
 
 
 def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
-    raise NotImplementedError
+    # If there's a winner, game is over
+    if winner(board) is not None:
+        return True
 
+    # If there's any empty cell left and no winner, game is not over
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] == EMPTY:
+                return False
+
+    # If there are no empty cells, game is over
+    return True
 
 def utility(board):
     """
