ff34c23d71ae696f3460911bdfcc53fff90a6c46
Assaf Amitai <87387953+assafam@users.noreply.github.com>
2022-12-20 10:52:10 +0200

    Implement load_data

--- traffic/traffic.py
+++ traffic/traffic.py
@@ -58,7 +58,19 @@ def load_data(data_dir):
     be a list of integer labels, representing the categories for each of the
     corresponding `images`.
     """
-    raise NotImplementedError
+    images = []
+    labels = []
+
+    for category in range(NUM_CATEGORIES):
+        print(f"Reading files for category {category}")
+        for filename in os.listdir(os.path.join(data_dir, str(category))):
+            img = cv2.imread(os.path.join(data_dir, str(category), filename))
+            assert img is not None, f"Could not read image {os.path.join(data_dir, str(category), filename)}"
+            img = cv2.resize(img, (IMG_WIDTH, IMG_HEIGHT))
+            images.append(img)
+            labels.append(category)
+
+    return (images, labels)
 
 
 def get_model():
