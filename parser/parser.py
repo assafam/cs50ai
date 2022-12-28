ddf5e06dd038424e64e29977fa7849aafdc33361
Assaf Amitai <87387953+assafam@users.noreply.github.com>
2022-12-28 10:20:11 +0200

    Implement preprocess

--- parser/parser.py
+++ parser/parser.py
@@ -62,7 +62,12 @@ def preprocess(sentence):
     and removing any word that does not contain at least one alphabetic
     character.
     """
-    raise NotImplementedError
+    words = [
+        word.lower() for word in nltk.word_tokenize(sentence)
+        if any(c.isalpha() for c in word)
+        ]
+
+    return words
 
 
 def np_chunk(tree):
